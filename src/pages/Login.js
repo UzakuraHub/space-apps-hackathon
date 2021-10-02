93c37c0186371383b9a2a44edd92b95a71721127
mansurissa <mansurissa6@gmail.com>
2021-10-02 23:37:25 +0200

    feat(auth):login page design

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,54 +4,56 @@ import LoginIcon from '@material-ui/icons/AccountCircle';
 
 const Login = () => {
   return (
-    <Container maxWidth='sm'>
-      <Box
-        sx={{
-          textAlign: 'center',
-          fontSize: '32px',
-          fontWeight: 'bold'
-        }}
-      >
-        LOGIN
-      </Box>
-      <div>
-        <TextField
-          id='outlined-basic'
-          label='Email'
-          variant='outlined'
-          type='email'
-          fullWidth
-        />
-      </div>
-      <div>
-        <TextField
-          id='outlined-basic'
-          label='Password'
-          variant='outlined'
-          type='password'
-          margin='normal'
-          fullWidth
-        />
-      </div>
-      <div>
-        <Button
-          variant='contained'
-          startIcon={<LoginIcon />}
-          fullWidth
-          size='large'
+    <div className='mt-10'>
+      <Container maxWidth='sm'>
+        <Box
+          sx={{
+            textAlign: 'center',
+            fontSize: '32px',
+            fontWeight: 'bold'
+          }}
         >
-          Login
-        </Button>
-      </div>
-      <Box sx={{ textAlign: 'center' }} margin='normal'>
-        Or
-      </Box>
-      <div>
-        <Button variant='outlined' fullWidth size='large'>
-          Register
-        </Button>
-      </div>
-    </Container>
+          LOGIN
+        </Box>
+        <div>
+          <TextField
+            id='outlined-basic'
+            label='Email'
+            variant='outlined'
+            type='email'
+            fullWidth
+          />
+        </div>
+        <div>
+          <TextField
+            id='outlined-basic'
+            label='Password'
+            variant='outlined'
+            type='password'
+            margin='normal'
+            fullWidth
+          />
+        </div>
+        <div>
+          <Button
+            variant='contained'
+            startIcon={<LoginIcon />}
+            fullWidth
+            size='large'
+          >
+            LOGIN
+          </Button>
+        </div>
+        <Box sx={{ textAlign: 'center' }} margin='normal'>
+          Or
+        </Box>
+        <div>
+          <Button variant='outlined' fullWidth size='large'>
+            REGISTER
+          </Button>
+        </div>
+      </Container>
+    </div>
   );
 };
 
